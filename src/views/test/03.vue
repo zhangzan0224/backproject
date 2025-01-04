5144c990ff52611759d027c581503ccf5a2451c8
zhangzan0224 <zhangzan0224@126.com>
2025-01-04 19:30:26 +0800

    注释

--- src/views/test/03.vue
+++ src/views/test/03.vue
@@ -185,7 +185,32 @@ export default {
     };
   },
   methods: {
-    // 帮我写一个方法实现第一列根据district合并，第二列合并的前提是district是一样的
+    /**
+     * @param {Object} params -  row, column, rowIndex, columnIndex
+     * @returns {Object} - 合并后的行数 rowspan, colspan
+     * @description 合并表格单元格
+     *
+     * @example
+     * objectSpanMethod({row, column, rowIndex, columnIndex})
+     *  row: 当前行对象
+     *  column: 当前列对象
+     *  rowIndex: 当前行索引
+     *  columnIndex: 当前列索引
+     *  return { rowspan: 0, colspan: 0 }
+     *这个 objectSpanMethod 方法是用于合并表格中的行的。它根据某些条件来决定是否合并行。
+
+    *    这个方法接收两个参数，rowIndex 和 columnIndex，分别代表当前处理的行和列。
+
+    当 columnIndex 为 0 时，它检查当前行的 district 是否与上一行相同。如果相同，则合并当前行的第一列，返回 { rowspan: 0, colspan: 0 }，从而隐藏当前行的单元格。
+
+    *如果不同，则计算连续相同 district 的行数，并返回 { rowspan, colspan: 1 }，从而合并行。
+
+    *当 columnIndex 为 1 时，它检查当前行的 sourceTypeDesc 是否与上一行相同，并且 scope 是否相同。如果相同，则合并当前行的第一列，返回 { rowspan: 0, colspan: 0 }。
+
+    *如果不同，则计算连续相同 sourceTypeDesc 和 scope 的行数，并返回 { rowspan, colspan: 1 }，从而合并行。
+
+    *对于其他列，它不进行合并，返回 { rowspan: 1, colspan: 1 }。
+    */
     objectSpanMethod ({ rowIndex, columnIndex }) {
       // 第一列如果district都一样进行合并
       if (columnIndex === 0) {
@@ -193,7 +218,8 @@ export default {
         const previousRow = this.tableData[rowIndex - 1]
         if (rowIndex > 0 && currentRow.district === previousRow.district) {
           // 如果当前行与上一行的 district 相同，合并当前行的第一列
-          return { rowspan: 0, colspan: 0 } // 当前行的 district 列不显示
+          // rowspan: 0, colspan: 0 - 当前行的 district 列不显示
+          return { rowspan: 0, colspan: 0 }
         }
         // 如果当前行与上一行的 district 不同，则显示当前行的 district
         let rowspan = 1
@@ -205,13 +231,15 @@ export default {
             break
           }
         }
-        return { rowspan, colspan: 1 } // 返回合并后的行数
+        // 返回合并后的行数
+        return { rowspan, colspan: 1 }
       } else if (columnIndex === 1) { // 针对 sourceTypeDesc 列进行判断
         const currentRow = this.tableData[rowIndex]
         const previousRow = this.tableData[rowIndex - 1]
         if (rowIndex > 0 && currentRow.sourceTypeDesc === previousRow.sourceTypeDesc && currentRow.scope === previousRow.scope) {
           // 如果当前行与上一行的 sourceTypeDesc 相同，合并当前行的第一列
-          return { rowspan: 0, colspan: 0 } // 当前行的 sourceTypeDesc 列不显示
+          // rowspan: 0, colspan: 0 - 当前行的 sourceTypeDesc 列不显示
+          return { rowspan: 0, colspan: 0 }
         }
         // 如果当前行与上一行的 sourceTypeDesc 不同，则显示当前行的 sourceTypeDesc
         let rowspan = 1
@@ -223,9 +251,11 @@ export default {
             break
           }
         }
-        return { rowspan, colspan: 1 } // 返回合并后的行数
+        // 返回合并后的行数
+        return { rowspan, colspan: 1 }
       }
-      return { rowspan: 1, colspan: 1 } // 其他列不合并
+      // 其他列不合并
+      return { rowspan: 1, colspan: 1 }
     }
     // objectSpanMethod({ rowIndex, columnIndex }) {
     //   if (columnIndex === 0) {
